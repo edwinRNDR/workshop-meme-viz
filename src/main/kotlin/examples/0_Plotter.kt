da88fe44449e097a5a51e8345f2ec46fb5e1cc02
Edwin Jakobs <edwin@rndr.studio>
2022-10-22 16:54:38 +0200

    Plotter

--- src/main/kotlin/examples/0_Plotter.kt
+++ src/main/kotlin/examples/0_Plotter.kt
@@ -1,10 +1,15 @@
 package examples
 
 import MemePlotter
+import library.Meme
 import library.loadLogits
 import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.writer
 import org.openrndr.extra.camera.Camera2D
+import org.openrndr.extra.color.presets.*
 import org.openrndr.math.Vector2
+import org.openrndr.shape.Rectangle
 import org.openrndr.shape.bounds
 import org.openrndr.shape.map
 
@@ -14,32 +19,66 @@ fun main() = application {
         height = 1080
     }
     program {
-        extend(Camera2D())
-        val tp = MemePlotter(12.0)
+        val camera = Camera2D()
+        extend(camera)
+        val tp = MemePlotter(12.0, 15.0)
 
-        val sizes = tp.memes.map { if (it.nsfw) 1.0 else 0.0 }
+        val sizes = tp.memes.map { if (it.nsfw) 1.0 else 0.5 }
+        val colors = tp.memes.map {
+            when (it.year) {
+                in 1990..2000 -> ColorRGBa.ORANGE_RED
+                in 2001..2006 -> ColorRGBa.CORNFLOWER_BLUE
+                in 2007..2012 -> ColorRGBa.LIGHT_SEA_GREEN
+                in 2013..2016 -> ColorRGBa.PURPLE
+                else -> ColorRGBa.PINK
+            }.opacify(0.5)
+        }
+
+        tp.positionsFile = "datasets/mood-tsne.csv"
+        tp.sizes = sizes
+        tp.colors = colors
+
+
+        var activeMemes = listOf<Meme>()
+        tp.plotterChange.listen { newMemes ->
+            activeMemes = newMemes
+        }
+
+        extend {
+            tp.draw(drawer, mouse.position)
+
+            drawer.defaults()
+            drawer.writer {
+                var y = 0.0
+                box = Rectangle(0.0, 0.0, 400.0, 400.0)
+                newLine()
+                activeMemes.forEach {
+                    it.toList().forEach {
+                        newLine()
+                        text(it)
+                    }
+                }
+            }
+
+            drawer.stroke = ColorRGBa.YELLOW
+            drawer.fill = null
+            drawer.circle(mouse.position, tp.queryRadius * (camera.view.trace - 1.0) / 3.0)
+        }
+    }
+}
+
+/*
 
 
         val logits = loadLogits("datasets/mood-logits-all.csv")
-
 
 //        val bla = logits["sexual"]!!.zip(logits["bad"]!!).map {
 //            Vector2(it.first, it.second)
 //        }
 
-        val bla = tp.memes.map { (it.year?:2000).toDouble().coerceAtLeast(2000.0) }.zip(logits["natural"]!!).map {
-            Vector2(it.first, -it.second)
-        }
-
-        val bounds = bla.bounds
-        val points = bla.map { it.map(bounds, drawer.bounds) }
-
-        tp.positions = points
-        //tp.positionsFile = "datasets/bert-tsne.csv"
-        //tp.sizes = sizes
-
-        extend {
-            tp.draw(drawer, mouse.position)
-        }
-    }
+val bla = tp.memes.map { (it.year?:2000).toDouble().coerceAtLeast(2000.0) }.zip(logits["natural"]!!).map {
+    Vector2(it.first, -it.second)
 }
+
+val bounds = bla.bounds
+val points = bla.map { it.map(bounds, drawer.bounds) }*/
--- src/main/kotlin/library/TilePlotter.kt
+++ src/main/kotlin/library/TilePlotter.kt
@@ -1,16 +1,20 @@
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import library.Meme
 import library.loadMemes
+import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.*
+import org.openrndr.events.Event
 import org.openrndr.extra.kdtree.kdTree
 import org.openrndr.math.Vector2
 import org.openrndr.math.Vector3
+import org.openrndr.math.Vector4
 import org.openrndr.shape.Rectangle
 import org.openrndr.shape.bounds
 import org.openrndr.shape.map
 import java.io.File
 import java.io.FileReader
 
-class MemePlotter(imageScale: Double = 10.0) {
+class MemePlotter(imageScale: Double = 10.0, val queryRadius: Double = 10.0) {
     val memeImage = loadImage("datasets/tiles-64.png")
 
     var positionsFile = "datasets/bert-umap.csv"
@@ -19,28 +23,42 @@ class MemePlotter(imageScale: Double = 10.0) {
             positions = loadData(value)
         }
 
+    private fun loadData(file: String): List<Vector2> {
+        val frame = Rectangle(0.0, 0.0,1080.0, 1080.0)
+        val pos = csvReader().readAll(File(file)).map {
+            Vector2(it[1].toDouble(), it[2].toDouble())
+        }
+        val mappedPositions = pos.map(pos.bounds, frame)
+        pointIndices = mappedPositions.indices.map { Pair(mappedPositions[it], it) }.associate { it }
+        kdtree = mappedPositions.kdTree()
+
+        return mappedPositions
+    }
+
     var positions = loadData(positionsFile)
         set(value) {
             field = value
-            kdtree = value.kdTree()
-            pointIndices = positions.indices.map { Pair(positions[it], it) }.associate { it }
-            positionInstances.apply {
-                put {
-                    for (pos in value) {
-                        write(pos)
-                    }
+            positionInstances.put {
+                for (pos in value) {
+                    write(pos)
                 }
             }
         }
     var sizes = positions.map { 1.0 }
         set(value) {
             field = value
-            sizeInstances.apply {
-                println("${positions.size}, ${value.size}")
-                put {
-                    for (size in value) {
-                        write(size.toFloat())
-                    }
+            sizeInstances.put {
+                for (size in value) {
+                    write(size.toFloat())
+                }
+            }
+        }
+    var colors = positions.map { ColorRGBa.WHITE }
+        set(value) {
+            field = value
+            colorsInstances.put {
+                for (c in value) {
+                    write(Vector4(c.r, c.g, c.b, c.alpha))
                 }
             }
         }
@@ -48,14 +66,6 @@ class MemePlotter(imageScale: Double = 10.0) {
     var kdtree = positions.kdTree()
     var pointIndices = positions.indices.map { Pair(positions[it], it) }.associate { it }
     var memes = loadMemes("datasets/memes-all.json")
-
-    private fun loadData(file: String): List<Vector2> {
-        val frame = Rectangle(0.0, 0.0,1080.0, 1080.0)
-        val pos = csvReader().readAll(File(file)).map {
-            Vector2(it[1].toDouble(), it[2].toDouble())
-        }
-        return pos.map(pos.bounds, frame)
-    }
 
     val quad = vertexBuffer(vertexFormat {
         position(3)
@@ -74,7 +84,7 @@ class MemePlotter(imageScale: Double = 10.0) {
         }
     }
 
-    val positionInstances = vertexBuffer(vertexFormat {
+    private val positionInstances = vertexBuffer(vertexFormat {
         attribute("offset", VertexElementType.VECTOR2_FLOAT32)
     }, positions.size).also {
         it.put {
@@ -83,13 +93,21 @@ class MemePlotter(imageScale: Double = 10.0) {
             }
         }
     }
-
-    val sizeInstances = vertexBuffer(vertexFormat {
+    private val sizeInstances = vertexBuffer(vertexFormat {
         attribute("size", VertexElementType.FLOAT32)
     }, positions.size).also {
         it.put {
             for (j in positions.indices) {
                 write(1.0f)
+            }
+        }
+    }
+    private val colorsInstances = vertexBuffer(vertexFormat {
+        attribute("color", VertexElementType.VECTOR4_FLOAT32)
+    }, positions.size).also {
+        it.put {
+            for (j in positions.indices) {
+                write(Vector4(1.0))
             }
         }
     }
@@ -105,7 +123,7 @@ class MemePlotter(imageScale: Double = 10.0) {
             float tx = mod((c_instance * 1.0) / itemsPerRow, 1.0) + va_texCoord0.x/itemsPerRow; 
             float ty = floor(c_instance / itemsPerRow)/itemsPerRow + va_texCoord0.y / itemsPerRow;
             ty = 1.0 - ty;
-            x_fill = texture(p_texture, vec2(tx, ty));
+            x_fill = mix(texture(p_texture, vec2(tx, ty)), vi_color.rgba, vi_color.a);
         """.trimIndent()
 
         parameter("texture", memeImage)
@@ -113,7 +131,16 @@ class MemePlotter(imageScale: Double = 10.0) {
         parameter("scale", imageScale.coerceAtLeast(1.0))
     }
 
-    var currentIndex = 0
+    var currentIndexes = listOf<Int>()
+        set(value) {
+            if(field != value) {
+                val nmemes = value.map { memes[it] }
+                field = value
+                plotterChange.trigger(nmemes)
+            }
+        }
+
+    var plotterChange = Event<List<Meme>>()
 
     fun draw(drawer: Drawer, mousePos: Vector2) {
         drawer.isolated {
@@ -121,32 +148,21 @@ class MemePlotter(imageScale: Double = 10.0) {
             drawer.shadeStyle = style
             drawer.vertexBufferInstances(
                 listOf(quad),
-                listOf(positionInstances, sizeInstances),
+                listOf(positionInstances, sizeInstances, colorsInstances),
                 DrawPrimitive.TRIANGLE_STRIP,
                 positionInstances.vertexCount
             )
 
 
+
             val cursorPosition = (drawer.view.inversed * mousePos.xy01).div.xy
-            val closest = kdtree.findNearest(cursorPosition)
+            val closest = kdtree.findAllInRadius(cursorPosition, 10.0)
             closest?.let {
-                val indexOfClosest = pointIndices[closest]
-                if(sizes[indexOfClosest!!] != 0.0) {
-                    currentIndex = indexOfClosest
-                }
+                val indexOfClosestOnes = pointIndices.filter { closest.contains(it.key) && sizes[it.value] != 0.0 }.map { it.value }
+                currentIndexes = indexOfClosestOnes
             }
 
-            style.parameter("currentIndex", currentIndex)
 
-            drawer.defaults()
-            drawer.writer {
-                box = Rectangle(0.0, 0.0, 400.0, 400.0)
-                newLine()
-                memes[currentIndex].toList().forEach {
-                    newLine()
-                    text(it)
-                }
-            }
         }
     }
 }
